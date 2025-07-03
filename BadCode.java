cfc61ef34711d8fc99d818254d79ac0fc4e98536
manisha-techcrumb <manisha@techcrumb.com>
2025-07-03 18:27:49 +0530

    Fix: trigger SonarQube on PR

--- BadCode.java
+++ BadCode.java
@@ -4,4 +4,31 @@ public class BadCode {
         System.out.println("Hello");
     }
 }
+public class BadCode {
+    public static void main(String[] args) {
+        String unusedVar = "This is unused"; // Unused variable
+
+        int x = 10 / 0; // Division by zero (bug)
+
+        if (true) {
+            if (true) {
+                if (true) {
+                    if (true) {
+                        if (true) {
+                            System.out.println("Too deep");
+                        }
+                    }
+                }
+            }
+        }
+
+        try {
+            int a = 5 / 0;
+        } catch (Exception e) {
+            // Empty catch block
+        }
+
+        String password = "admin123"; // Hardcoded secret
+    }
+}
 
