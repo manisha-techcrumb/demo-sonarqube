4ea4cec4c07a95b489deaccfe8770eec3545480d
manisha-techcrumb <manisha@techcrumb.com>
2025-07-03 17:58:17 +0530

    Add bad code to trigger SonarQube failure

---
+++ BadCode.java
@@ -0,0 +1,7 @@
+public class BadCode {
+    public static void main(String[] args) {
+        String unusedVar = "This is unused"; // Unused variable (issue)
+        System.out.println("Hello");
+    }
+}
+
