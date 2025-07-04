4582e8419c60691235a5b617777559f0b3620dc0
manisha-techcrumb <manisha@techcrumb.com>
2025-07-04 12:58:51 +0530

    fixed the issue

--- src/main/java/com/example/Dummy.java
+++ src/main/java/com/example/Dummy.java
@@ -1,8 +1,12 @@
 package com.example;
 
+import java.util.logging.Logger;
+
 public class Dummy {
+    private static final Logger logger = Logger.getLogger(Dummy.class.getName());
+
     public void sayHello() {
-        System.out.println("Hello, clean world!");
+        logger.info("Hello, clean world!");
     }
 }
 
