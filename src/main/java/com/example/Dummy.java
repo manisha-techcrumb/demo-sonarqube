5139ba611a6b688bc35be8c69f66fbcbf682ceaa
manisha-techcrumb <manisha@techcrumb.com>
2025-07-04 12:44:35 +0530

    correct code

--- src/main/java/com/example/Dummy.java
+++ src/main/java/com/example/Dummy.java
@@ -1,10 +1,10 @@
 package com.example;
 
 public class Dummy {
-    public void badMethod() {
-        if (true)
-            System.out.println("This is a bad practice"); // Noncompliant
-        int a = 1 / 0; // Noncompliant
+
+    public void printMessage() {
+        String message = "Hello, SonarQube is working!";
+        System.out.println(message);
     }
 }
-}}
+
